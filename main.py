76c175f00ce4b2995d27b161e4b3b7ce05b22af2
ManuGZ <galonso097@gmail.com>
2025-07-07 13:04:28 -0500

    feat: configure CORS middleware with specific origins

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 from adapters.bpm.bpm_processor import router as bpm_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-allow_origins = ["*"]
+origins = [
+    "http://localhost:4200/",
+    # Puedes agregar más orígenes si necesitas
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # puedes usar [""] para desarrollo si no tienes riesgos
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 
 # Registrar el router de BPM
 app.include_router(bpm_router)
