c11712166b485dbc79049441d32b31b61ac5e2d9
ManuGZ <galonso097@gmail.com>
2025-07-07 13:07:31 -0500

    fix: update CORS middleware to allow all origins

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # puedes usar [""] para desarrollo si no tienes riesgos
+    allow_origins=["*"],  # puedes usar [""] para desarrollo si no tienes riesgos
     allow_credentials=True,
     allow_methods=[""],
     allow_headers=["*"],
